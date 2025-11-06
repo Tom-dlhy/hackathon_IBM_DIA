557b7c83e15f76586d6ddd1554e22425e0e554af
GabrielCarlotti <148062188+GabrielCarlotti@users.noreply.github.com>
2025-11-06 16:03:06 +0100

    badge gab

--- test.py
+++ test.py
@@ -1,21 +1,47 @@
 from ibm_watsonx_ai import Credentials
-from ibm_watsonx_ai.foundation_models import Model
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai.foundation_models.utils.enums import ModelTypes, DecodingMethods
+from dotenv import load_dotenv
+import os
+import asyncio
+load_dotenv()
+
+IBM_API_KEY = os.getenv("IBM_API_KEY")
+URL=os.getenv("URL")
+PROJECT_ID=os.getenv("PROJECT_ID")
+
+print("IBM_API_KEY:", IBM_API_KEY)
+print("URL:", URL)
+print("PROJECT_ID:", PROJECT_ID)
 
 creds = Credentials(
-    api_key="TON_API_KEY",
-    url="https://eu-de.ml.cloud.ibm.com"  # ou autre région affichée
+    api_key=IBM_API_KEY, 
+    url=URL 
 )
 
-model = Model(
-    model_id="mistralai/mixtral-8x7b-instruct",   # choisi dans Model Garden
+client = APIClient(creds, project_id=PROJECT_ID)
+
+# To display example params enter
+GenParams().get_example_values()
+
+generate_params = {
+    GenParams.MAX_NEW_TOKENS: 25
+}
+
+model_inference = ModelInference(
+    model_id='mistralai/mistral-medium-2505',
+    params=generate_params,
     credentials=creds,
-    project_id="TON_PROJECT_ID"                    # affiché dans watsonx.ai
-)
+    project_id=PROJECT_ID
+)   
 
-result = model.generate_text(
-    prompt="Explique la différence entre embeddings et LLM en 5 lignes.",
-    max_new_tokens=120,
-    temperature=0.7
-)
+async def test_inference():
+    response = await model_inference.agenerate(
+        prompt="Write a short poem about the sea."
+    )
+    print(response)
 
-print(result)
+if __name__ == "__main__":
+    asyncio.run(test_inference())
